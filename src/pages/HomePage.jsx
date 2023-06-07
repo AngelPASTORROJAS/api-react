da53afe45fae597887bd78d65ea4494859bb7dd9
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-07 10:27:50 +0200

    feat: ajout d'un exemple de l'utilisation de fetch pour récuperer des donées api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
+
 const HomePage = () => {
-  return (
-    <>
-    </>
-  );
+  const [animals, setAnimals] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos/1")
+      .then((response) => response.json())
+      .then((response) => {
+        setAnimals(response);
+      });
+    //Il ne se lance qu'sau chargement du composant
+  }, []);
+  return <>
+  <img src={animals.url} alt="" />
+  <p>{animals.title}</p>
+  </>;
 };
 export default HomePage;
