923449e045ea45d472d384ad7c8e0cf803168d8a
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-07 09:39:58 +0200

    feat: ajout de la page home et la page 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+          <HomePage />
+      ),
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
   return (
-    <>
-    </>
-  )
+      <RouterProvider router={router} />
+  );
 }
-
-export default App
+export default App;
